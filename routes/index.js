e22283b8feb491c2b112ff9f46ce3d62db82f099
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-04-02 17:18:02 +0100

    add front routs

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get('/university', (request, response) => {
     //GO SEARCH THE UNIVERSITY INFOS & ALL IT'S RATINGS (GET REQUEST)
     response.render('main', {layout:'university'})
 })
-
+router.get('/rateauth', (request, response) => {
+    response.render('main', {layout:'rateauth'})
+})
+router.get('/rateform', (request, response) => {
+    response.render('main', {layout:'rateform'})
+})
 //BACK ROUTING
 module.exports = router;
